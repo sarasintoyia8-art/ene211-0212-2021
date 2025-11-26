2c58f74bec2db94ee9ce2bfb7a8977eb0bef15a9
sarasintoyia8-art <sarasintoyia8@gmail.com>
2025-11-26 15:26:14 +0300

    Add files via upload

---
+++ assignment1/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 3;
+
+    printf("First number: 10\n");
+    printf("Second number: 3\n\n");
+
+    printf("Addition: 10 + 3 = 13\n");
+    printf("Subtraction: 10 - 3 = 7\n");
+    printf("Multiplication: 10 * 3 = 30\n");
+    printf("Division: 10 / 3 = 3\n");
+    printf("Modulus: 10 %% 3 = 1\n");
+
+    return 0;
+}
+
+
+
+
